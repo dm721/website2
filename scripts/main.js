b813849fcc8b12ca4aeae296934d1791a907a5f9
Fuck you <dmckenna721@gmail.com>
2023-12-25 23:57:35 -0500

    Updated viewports and shit

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+documentHeight();
 play();
+checkVideo();
+
+function checkVideo() {
+  window.onload = function(){
+    const video = document.getElementById("myVideo");
+    video.addEventListener("loadeddata", (event) => {
+      alert("video loaded");
+    });
+  }
+}
 
 function play() {
-    var audio = new Audio('images/music.mp3');
-    audio.loop = true;
-    audio.play();
-  }
+  var audio = new Audio('images/music.mp3');
+  audio.loop = true; 
+  audio.play();
+  audio.volume = 0.1;
+}
+
+function documentHeight() {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+  alert('test');
+ }
+
